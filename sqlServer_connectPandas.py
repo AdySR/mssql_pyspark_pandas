0b86c5332c1ed048698e228298f72ea8d2542dc1
Adatix <rathi.ady@gmail.com>
2022-05-30 01:03:29 +0530

    Added new file for pyspark, renamed existing file removed pyspark from it

---
+++ sqlServer_connectPandas.py
@@ -0,0 +1,22 @@
+import pypyodbc as pyodbc
+import pandas as pd
+
+
+ServerName = 'ICEN472'
+DatabaseName = 'feederDB'
+
+conn = pyodbc.connect("""
+    Driver={{SQL Server Native Client 11.0}};
+    Server={0};
+    Database={1};
+    Trusted_Connection=yes;
+""".format(ServerName, DatabaseName))
+
+sql_query ="""
+SELECT TOP (1000) [id]
+  FROM [dbo].[adityaTest]
+"""
+
+df_sql_query = pd.read_sql(sql_query,conn)
+
+print(df_sql_query.head(10))
